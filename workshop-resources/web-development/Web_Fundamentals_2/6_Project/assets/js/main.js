550da4766683700979b91c94fe74154ec9f265bb
KimberlyBolton1 <64802674+KimberlyBolton1@users.noreply.github.com>
2020-08-14 09:06:11 -0700

    Update main.js
    Completed copyedit on comments.

--- workshop-resources/web-development/Web_Fundamentals_2/6_Project/assets/js/main.js
+++ workshop-resources/web-development/Web_Fundamentals_2/6_Project/assets/js/main.js
@@ -1,16 +1,16 @@
-// Verify that the Javascript is properly linked by logging to the console.
+// Verify that the JavaScript is properly linked by logging in to the console.
 // console.log("main.js")
 
 // Step 0: Setup
 document.addEventListener("DOMContentLoaded", () => {
   /*
-    First thing we should do is wait until html is done loading before running our Javascript.
+    First thing to do is wait until HTML finishes loading before running JavaScript.
     This is especially important when using the document.querySelector function.
-    If the html element we are trying to select is not yet loaded,
-    we will not be able to find the element on the page.
+    If the HTML element you are trying to select is not yet loaded,
+    you won't be able to find the element on the page.
   */
 
-  // Create event listener for the button to find jobs
+  // Create event listener for the button to find jobs.
   document
     .querySelector("#find-jobs")
     .addEventListener("click", appendJobsToPage)
@@ -20,59 +20,59 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Function 1 appendJobsToPage
 async function appendJobsToPage() {
-  // Using an async function we can wait for the data to return before proceeding with the code execution
-  // Step 1: fetch the data
+  // Using an async function, you can wait for the data to return before proceeding with the code execution.
+  // Step 1: fetch the data.
   const postsData = await getPostsData()
   const usersData = await getUsersData()
 
-  // Step 2: remove tip element since we have our post and user data
+  // Step 2: remove tip element because you have your post and user data.
   const tip = document.querySelector("#tip")
   tip.remove()
 
-  // Step 3: Select the postings element in preparation for appending the posts to the page
+  // Step 3: Select the postings element in preparation for appending the posts to the page.
   const postings = document.querySelector("#postings")
 
-  // Step 4: Loop through each post
+  // Step 4: Loop through each post.
   for (post of postsData) {
-    // Log the post to inspect the object in the browser console
+    // Log the post to inspect the object in the browser console.
     // console.log(`post: `, post)
 
-    // Step 5: Find the user who created the post
+    // Step 5: Find the user who created the post.
     let user = usersData.filter((user) => user.id === post.userId)
     /*
-      Since the filter function returns an array of matching users we can set the
-      user variable equal to the user in the first position of the array
+      Because the filter function returns an array of matching users, you can set the
+      user variable equal to the user in the first position of the array.
     */
     if (user.length === 1) {
       user = user[0]
     } else {
-      // If we don't find the user
-      // throw an error
+      // If you don't find the user,
+      // throw an error.
       throw new Error("No user found")
     }
 
-    // Log the user to inspect the object in the browser console
+    // Log the user to inspect the object in the browser console.
     // console.log(`user: `, user)
 
-    // Now that we have a user we can build the html
+    // Now that you have a user, you can build the HTML.
 
-    // Step 6: Using javascript create the html
-    // The example html we are going to be build can be found in the index.html file
+    // Step 6: Use JavaScript create the HTML.
+    // The example HTML you will build can be found in the index.html file.
 
-    // Create a div element to hold our job posting
+    // Create a div element to hold your job posting.
     const divNode = document.createElement("div")
     divNode.className = "job-post"
 
-    // Create a h3 element to hold the job title
+    // Create an H3 element to hold the job title.
     const h3Node = document.createElement("h3")
-    // Using template literals we can create a string that includes some text and the post title
+    // Using template literals, you can create a string that includes some text and the post title.
     const jobTitle = document.createTextNode(`Job Title: ${post.title}`)
-    // Add the text node to the h3 element
+    // Add the text node to the H3 element.
     h3Node.appendChild(jobTitle)
-    // Add the h3 node to the div element
+    // Add the H3 node to the div element.
     divNode.appendChild(h3Node)
 
-    // Repeat for each element
+    // Repeat for each element.
 
     const h4Node = document.createElement("h4")
     const company = document.createTextNode(`Company: ${user.company.name}`)
@@ -103,19 +103,19 @@ async function appendJobsToPage() {
     pContactNode.appendChild(moreDetailsText)
     divNode.appendChild(pContactNode)
 
-    // Step 7: Appending html to page
+    // Step 7: Append HTML to the page.
     postings.appendChild(divNode)
   }
   // End of for loop
 
   // Done!
-  // By now the page should have the code for each post that was returned by the API
-  // Confirm in your browser and inspect the html to verify that the javascript generated html is valid
+  // By now, the page should have the code for each post that was returned by the API.
+  // Confirm this in your browser and inspect the HTML to verify that the JavaScript-generated HTML is valid.
 }
 
 /* Function 2 getUsersData */
 async function getUsersData() {
-  // Fetch users using the browser fetch API
+  // Fetch users using the browser fetch API.
   const usersResponse = await fetch(
     "https://jsonplaceholder.typicode.com/users",
     {
@@ -123,10 +123,10 @@ async function getUsersData() {
       headers: { "Access-Control-Allow-Origin": "*" },
     }
   )
-  // Convert the response into json
+  // Convert the response into JSON.
   const usersData = await usersResponse.json()
 
-  // Log the data to see what it looks like
+  // Log the data to see how it appears.
   // console.log(`usersData: `, usersData)
 
   return usersData
@@ -134,7 +134,7 @@ async function getUsersData() {
 
 /* Function 3 getPostsData */
 async function getPostsData() {
-  // Fetch posts using the browser fetch API
+  // Fetch posts using the browser fetch API.
   const postsResponse = await fetch(
     "https://jsonplaceholder.typicode.com/posts",
     {
@@ -142,10 +142,10 @@ async function getPostsData() {
       headers: { "Access-Control-Allow-Origin": "*" },
     }
   )
-  // Convert the response into json
+  // Convert the response into JSON.
   const postsData = await postsResponse.json()
 
-  // Log the data to see what it looks like
+  // Log the data to see how it appears.
   // console.log(`postsData: `, postsData)
 
   return postsData
